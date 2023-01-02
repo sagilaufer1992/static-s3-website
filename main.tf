7b7b007fff12abbf3197ab387223f90097a2b4b7
Sagi Laufer <sagi.laufer@env0.com>
2023-01-02 15:07:06 +0200

    tidy up main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "aws" {
-  region  = "us-east-1"
-}
-
 terraform {
   required_version = ">= 1.2"
-
-  required_providers {
-    aws = "~> 2.0"
-  }
 }
--- modules/s3-bucket-website/main.tf
+++ modules/s3-bucket-website/main.tf
@@ -1,19 +1,9 @@
-provider "random" {}
-
-resource "random_string" "random" {
-  length = 16
-  special = false
-  min_lower = 16
+provider "aws" {
+  region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = "${var.bucket_name_prefix}-${random_string.random.result}"
-  acl    = "public-read"
-
-  force_destroy = true
-
-  website {
-    index_document = var.index_file_path
-    error_document = var.index_file_path
+terraform {
+  required_providers {
+    aws = "~> 2.0"
   }
 }
