b3554784a300e86e67c79a4c8b6e0898caf95627
Sagi Laufer <sagi.laufer@env0.com>
2023-01-02 11:03:37 +0200

    moving provider version into required_providers block

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
 provider "aws" {
-  version = "~> 2.0"
   region  = "us-east-1"
 }
 
 terraform {
   required_version = ">= 1.2"
+
+  required_providers {
+    aws = "~> 2.0"
+  }
 }
 
 provider "random" {}
