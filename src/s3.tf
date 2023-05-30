24e2dd5d07928e3d5ea9baae7c8473dba5eb58ec
Sagi Laufer <sagi.laufer@env0.com>
2023-05-30 12:34:47 +0300

    moved modules to a sibling directory

--- src/s3.tf
+++ src/s3.tf
@@ -1,5 +1,5 @@
 module "s3-website" {
-  source = "./modules/s3-bucket-website"
+  source = "../modules/s3-bucket-website"
   bucket_name_prefix = "static-website"
   index_file_path = "index.html"
 }
