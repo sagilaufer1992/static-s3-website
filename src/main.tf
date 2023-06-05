624b15d1032fef3f5efc36e496bf99fa7a1bf19b
Sagi Laufer <sagi.laufer@env0.com>
2023-06-05 12:02:03 +0300

    remote backend in env0

--- src/main.tf
+++ src/main.tf
@@ -1,3 +1,20 @@
 terraform {
   required_version = ">= 1.2"
+  backend "remote" {
+    hostname     = "backend-pr12195.api.dev.env0.com"
+    organization = "70479eba-7711-416f-9396-a41ed9ec07ba"
+    workspaces {
+      name = "sibling-dir"
+    }
+  }
 }
+
+#terraform {
+#  backend "remote" {
+#    organization = "sagi-env0"
+#
+#    workspaces {
+#      name = "static-website"
+#    }
+#  }
+#}
