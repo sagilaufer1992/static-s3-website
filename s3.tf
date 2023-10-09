21451c491d218a141eb8157a54fbd0edeca5dd9c
Sagi Laufer <sagi.laufer@env0.com>
2023-10-09 14:11:09 +0300

    switched module to another private module

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 module "static-website-s3" {
-  source = "api-dev.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/static-website-s3/env0rnd"
+  source = "api-dev.dev.env0.com/a3be99f0-f699-407d-8d23-ba9df6838fe3/static-web-site/sagienv0"
   version = "1.0.0"
   bucket_name_prefix = "static-website"
   index_file_path = "index.html"
