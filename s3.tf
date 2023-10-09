b20c13da1202d449c1bc0554c2f1b5d9ffb34874
sagilaufer1992 <sagi.laufer@env0.com>
2023-10-09 14:58:38 +0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 module "static-website-s3" {
-  source = "api-dev.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/static-website-s3/env0rnd"
+  source = "api-pr13342.dev.env0.com/8b1bc07e-f313-4302-bd0e-23f8fe04ad42/pr13342-static-web/sagiqa"
   version = "1.0.0"
   bucket_name_prefix = "static-website"
   index_file_path = "index.html"
