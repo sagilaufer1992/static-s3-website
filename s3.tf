45b74e4f6f51d22d1bedad2f8a91c6c541edd2db
sagilaufer1992 <sagi.laufer@env0.com>
2025-01-20 14:44:28 +0200

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,3 @@
 module "static-website-s3" {
-  source = "api-pr17956.dev.env0.com/737bb6df-1d96-4cec-825b-1e957dc54b95/fsfghgdfssdfgfgd/cxvzxcvxzcvzxcvzx"
-  version = "1.0.0"
-  bucket_name_prefix = "static-website"
-  index_file_path = "index.html"
+  source = "Invicton-Labs/uuid/random"
 }
